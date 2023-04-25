7c25493ec6ed8d4629a0dbc453aaa691cff7f3f2
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2023-04-25 14:32:06 +0200

    Expose entity IDs

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/sbecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/sbecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.ecommerce.sbecommerce.config;
 
 import com.ecommerce.sbecommerce.entity.Product;
 import com.ecommerce.sbecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import org.hibernate.type.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    // This constructor will be used by Spring to inject the EntityManager
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,6 +42,25 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        // call an internal helper method
+        exposeIds(config);
+    }
 
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+        // - get a list of all entity classes from the entity manager
+        var entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (var entity : entities) {
+            entityClasses.add(entity.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
