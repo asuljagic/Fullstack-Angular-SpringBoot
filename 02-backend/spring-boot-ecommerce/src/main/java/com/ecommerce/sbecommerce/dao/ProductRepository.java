37e5600889aab4f9eda9d82b2c51bda6750d206f
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2023-04-25 13:59:53 +0200

    Modify REST Repository with new method

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/sbecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ecommerce/sbecommerce/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.ecommerce.sbecommerce.dao;
 
 import com.ecommerce.sbecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200") // This annotation will allow Angular to access the endpoint at http://localhost:8080/api/products
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable); // This method will be used to get the products by category id
 }
