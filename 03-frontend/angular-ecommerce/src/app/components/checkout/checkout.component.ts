c95012084b5623068b4ef69bef85fdff50ceea2f
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2023-05-01 18:45:48 +0200

    Update template to display error messages

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -85,10 +85,11 @@ export class CheckoutComponent implements OnInit {
         this.countries = data;
       }
     );
-
-
-
   }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
 
   onSubmit() {
     console.log("Handling the submit button");
